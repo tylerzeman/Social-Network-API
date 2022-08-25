7d25f0910384185f4da96fc7520022fe4266a3ce
Tyler Zeman <tylerzeman1999@gmai9l.com>
2022-08-24 22:34:01 -0500

    added ability to add a user to the database

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,4 +1,23 @@
 const api_router = require('express').Router();
-const { User, Thought, Reaction } = require('../models');
+const  User = require('../models/user');
+const bcrypt = require('bcrypt');
+const { db } = require('../models/user');
 
-api_router.get()
+api_router.get('/users', async (req, res) => {
+    const users = await User.find();
+    res.send(users);
+});
+
+api_router.post('/user', async (req, res) => {
+    const req_pass = await bcrypt.hash(req.body.password, 10);
+    req.body.password = req_pass;
+    const createUser = await User.create(req.body);
+    res.send(createUser);
+});
+
+api_router.put('/user:id', async (req, res) => {
+   const updateUser = await User.updateOne(req.body);
+   res.send(updateUser)
+})
+
+module.exports = api_router;
